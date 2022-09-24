54ad4c4eb991b6134f1ce3617d51f5c9bd12bb07
lblblong <1162275271@qq.com>
2022-09-24 21:23:36 +0800

    切换为hash路由

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,9 @@
-import { Router } from 'oh-router'
+import { createHashHistory, Router } from 'oh-router'
 import { editorRoute } from 'src/pages/editor/route'
 import { indexRoute } from 'src/pages/index/route'
 
 export const router = new Router<any>({
+  history: createHashHistory(),
   routes: [
     indexRoute,
     editorRoute,

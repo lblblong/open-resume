20113d59875fd98aba2f2340bdf88b7553aa3073
lblblong <1162275271@qq.com>
2022-09-25 11:08:57 +0800

    修改默认数据、模板预览图片

--- src/shared/formily/datas/developer/index.ts
+++ src/shared/formily/datas/developer/index.ts
@@ -8,9 +8,9 @@ const DEFAULT_DATA = {
   phoneNumber: '18888888888',
   email: '1162275271@qq.com',
   education: {
-    schoolName: '某某某学校',
+    schoolName: '霍格沃兹',
     graduationTime: '2022',
-    major: '计算机系',
+    major: '黑魔法防御系',
   },
   target: '前端 & NodeJS',
   introduce: `独立使用 React/Vue 开发过各端产品，包括但不限于：Web 应用、微信小程序、Electron 桌面应用、React Native 等。\n用 Java/Kotlin(SpringBoot、MyBatis) 写过后端，现在更熟悉 Node.js(nest/egg/koa)。\n用 Java/Kotlin 和 Flutter 写过安卓。\n独立开发者：从零到一上线过多款个人产品，有网页、安卓APP、公众号应用，小程序等。`,
@@ -21,22 +21,22 @@ const DEFAULT_DATA = {
         'Taro 小程序路由库/自动生成带参数类型提示的路由方法/允许传递任意类型、任意大小的参数数据/同步的路由方法调用/ koa 体验一致的路由中间件。收录在 Taro 官方推荐物料中，正在为仓鼠星球的小程序端路由提供服务，运行稳定。',
     },
     {
-      title: 'tarojs-router-next - Taro 路由库（★160+）',
+      title: 'open-resume - 在线简历系统',
       content:
-        'Taro 小程序路由库/自动生成带参数类型提示的路由方法/允许传递任意类型、任意大小的参数数据/同步的路由方法调用/ koa 体验一致的路由中间件。收录在 Taro 官方推荐物料中，正在为仓鼠星球的小程序端路由提供服务，运行稳定。',
+        '纯前端在线简历编辑生成工具、支持导入导出配置、生成 PDF 和图片',
     },
   ],
   github: 'github.com/lblblong',
   workingHistory: [
     {
-      title: '某某公司 - 前端开发工程师',
-      content: `• XXX 项目\n负责该项目网站及移动端开发，该项目主要使用 React + Mobx，我独立负责了该项目的 XXX、XXX 和 XXX 模块，在项目上线后运行良好，期间除了一些兼容性问题没有出现过任何代码逻辑上的 BUG。\n• 其他工作\n负责考试报名 APP 以及管理后台的维护升级、公司推广落地页的编写、其他前端工作。`,
+      title: '神奇动物管理控制司 - 前端开发工程师',
+      content: `• 全球神奇动物大数据系统\n负责该项目网站及移动端开发，该项目主要使用 React + Mobx，我独立负责了该项目的神奇动物搜索和录入信息模块，在项目上线后运行良好，期间除了一些兼容性问题没有出现过任何代码逻辑上的 BUG。\n• 其他工作\n负责司内部分数据中台的开发和运维、神奇动物推广落地页的编写、其他前端工作。`,
       startTime: '2016.12',
       endTime: '2022.09',
     },
     {
-      title: '某某公司 - 前端开发工程师',
-      content: `• XXX 项目\n负责该项目网站及移动端开发，该项目主要使用 React + Mobx，我独立负责了该项目的 XXX、XXX 和 XXX 模块，在项目上线后运行良好，期间除了一些兼容性问题没有出现过任何代码逻辑上的 BUG。\n• 其他工作\n负责考试报名 APP 以及管理后台的维护升级、公司推广落地页的编写、其他前端工作。`,
+      title: '预言家日报 - NodeJS',
+      content: `• 《预言家日报》\n负责该项目后端的开发，众所周知该报纸的特色是动态的页面，其实现原理是通过魔法在报纸中嵌入了一个 webview。我在该项目中负责报纸客户端相关接口的开发。`,
       startTime: '2016.12',
       endTime: '2022.09',
     },
--- src/shared/formily/datas/normal-sidebar/index.ts
+++ src/shared/formily/datas/normal-sidebar/index.ts
@@ -10,14 +10,14 @@ const DEFAULT_DATA = {
   introduce: `独立使用 React/Vue 开发过各端产品，包括但不限于：Web 应用、微信小程序、Electron 桌面应用、React Native 等。\n用 Java/Kotlin(SpringBoot、MyBatis) 写过后端，现在更熟悉 Node.js(nest/egg/koa)。\n用 Java/Kotlin 和 Flutter 写过安卓。\n独立开发者：从零到一上线过多款个人产品，有网页、安卓APP、公众号应用，小程序等。`,
   workingHistory: [
     {
-      title: '某某公司 - 前端开发工程师',
-      content: `• XXX 项目\n负责该项目网站及移动端开发，该项目主要使用 React + Mobx，我独立负责了该项目的 XXX、XXX 和 XXX 模块，在项目上线后运行良好，期间除了一些兼容性问题没有出现过任何代码逻辑上的 BUG。\n• 其他工作\n负责考试报名 APP 以及管理后台的维护升级、公司推广落地页的编写、其他前端工作。`,
+      title: '神奇动物管理控制司 - 前端开发工程师',
+      content: `• 全球神奇动物大数据系统\n负责该项目网站及移动端开发，该项目主要使用 React + Mobx，我独立负责了该项目的神奇动物搜索和录入信息模块，在项目上线后运行良好，期间除了一些兼容性问题没有出现过任何代码逻辑上的 BUG。\n• 其他工作\n负责司内部分数据中台的开发和运维、神奇动物推广落地页的编写、其他前端工作。`,
       startTime: '2016.12',
       endTime: '2022.09',
     },
     {
-      title: '某某公司 - 前端开发工程师',
-      content: `• XXX 项目\n负责该项目网站及移动端开发，该项目主要使用 React + Mobx，我独立负责了该项目的 XXX、XXX 和 XXX 模块，在项目上线后运行良好，期间除了一些兼容性问题没有出现过任何代码逻辑上的 BUG。\n• 其他工作\n负责考试报名 APP 以及管理后台的维护升级、公司推广落地页的编写、其他前端工作。`,
+      title: '预言家日报 - NodeJS',
+      content: `• 《预言家日报》\n负责该项目后端的开发，众所周知该报纸的特色是动态的页面，其实现原理是通过魔法在报纸中嵌入了一个 webview。我在该项目中负责报纸客户端相关接口的开发。`,
       startTime: '2016.12',
       endTime: '2022.09',
     },
@@ -25,7 +25,7 @@ const DEFAULT_DATA = {
   sidebar: [
     {
       title: '教育背景',
-      content: `清华大学\n本科\n计算机\n2014-2018`,
+      content: `霍格沃兹\n黑魔法防御系\n2014-2018`,
     },
     {
       title: '求职意向',
